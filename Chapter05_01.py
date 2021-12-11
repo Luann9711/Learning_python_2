8c2475adcd40d4ac73d26ee4acba6ff8664bdbbb
luann9711 <marchen9708@gmail.com>
2021-12-11 16:56:57 +0900

    reduce 함수 / lambda 함수

--- Chapter05_01.py
+++ Chapter05_01.py
@@ -35,3 +35,36 @@ print(var_func(10))
 
 # 함수 인수 전달 및 함수로 결과 반환 -> 고위함수(Higher-order function)
 # map, filter, reduce 등
+print(list(map(var_func, filter(lambda x: x % 2, range(1, 6)))))
+print([var_func(i) for i in range(1, 6) if i % 2])
+
+# reduce 함수  - 누적값 계산에 사용
+from functools import reduce
+from operator import add
+
+print(reduce(add, range(1,11)))
+print(sum(range(1,11)))
+
+# 익명함수(lambda)
+# 가급적 주석 활용
+# 가급적 함수 작성
+# 일반 함수 형태로 리팩토링 권장
+
+print(reduce(lambda x, t: x + t, range(1,1025)))
+
+# Callable : 호출 연산자 - > 메소드 형태로 호출가능한지 확인
+# 호출 가능 확인
+print(callable(str), callable(len))
+
+# partial 사용법 : 인수고정 -> 콜백함수 사용
+from operator import mul
+from functools import partial
+
+print(mul(10, 10))
+
+# 인수 고정
+
+five  = partial(mul, 5)
+print(five(14))
+
+print([five(i) for i in range(1, 257)])
